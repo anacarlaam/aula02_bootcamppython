00201e83715c5c653563be048177ccb44e68d0fd
anacarlaam <anacarlaam@gmail.com>
2025-03-02 00:25:06 -0300

    adicionado exercicio 14

--- exercicios.py
+++ exercicios.py
@@ -4,10 +4,15 @@
 #num2=int(input("Inserir um numero inteiro: "))
 #resultado=num1//num2
 #print(resultado)
-## Ex 10
 
-import math 
-raio =float(input("Digite o raio:"))
-pi=math.pi
-area=pi*raio**2
-print(f"{area:.2f}")
+## Ex 10
+#import math 
+#raio =float(input("Digite o raio:"))
+#pi=math.pi
+#area=pi*raio**2
+#print(f"{area:.2f}")
+
+#EX14
+data_usuario=input("Digite uma data (dd/mm/aaaa): ")
+data = data_usuario.split("/")
+print("dia: ", data[0],"\nMes: ",data[1],"\nAno: ", data[2])
