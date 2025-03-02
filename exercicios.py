17e6d65c7288b33424f21725827f44d32b47231f
anacarlaam <anacarlaam@gmail.com>
2025-03-01 23:21:13 -0300

    adicionado exercicio 10

--- exercicios.py
+++ exercicios.py
@@ -1,6 +1,13 @@
-##/*Exerc04*/
+##/*Exerc04
+##
+# num1=int(input("Inserir um numero inteiro: "))
+#num2=int(input("Inserir um numero inteiro: "))
+#resultado=num1//num2
+#print(resultado)
+## Ex 10
 
-num1=int(input("Inserir um numero inteiro: "))
-num2=int(input("Inserir um numero inteiro: "))
-resultado=num1//num2
-print(resultado)
+import math 
+raio =float(input("Digite o raio:"))
+pi=math.pi
+area=pi*raio**2
+print(f"{area:.2f}")
